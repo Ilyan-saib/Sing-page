4e2dea040fa792e5490967b127f523a536c3e3be
CannelleJP <isaibmezghiche@edenschool.fr>
2023-01-06 16:04:09 +0100

    jsp

---
+++ main.js
@@ -0,0 +1,41 @@
+let left = document.getElementById('left')
+let right = document.getElementById('right')
+let image = document.getElementById('image')
+let counteur = 3
+
+left.addEventListener('click', ()=>{
+  if (counteur <= 1) {
+    left.style.backgroundColor = 'grey'
+    left.style.borderColor = 'grey'
+    
+  }
+  else{
+    right.style.backgroundColor = 'black'
+    right.style.borderColor = 'black'
+  counteur--
+  image.src = `/assets/image-slide-${counteur}.jpg`
+
+  }
+  
+
+});
+
+
+
+right.addEventListener('click', ()=>{
+    if (counteur >= 5) {
+      right.style.backgroundColor = 'grey'
+      right.style.borderColor = 'grey'
+     
+      
+    }
+    else{
+        left.style.backgroundColor = 'black'
+        left.style.borderColor = 'black'
+    counteur++
+    image.src = `/assets/image-slide-${counteur}.jpg`
+  
+    }
+    
+  
+  });
